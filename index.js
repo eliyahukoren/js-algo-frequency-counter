318fee4df4b337bbb112f57b475f81fe265850c0
eliyahukoren <elik.koren@gmail.com>
2022-12-31 22:35:33 +0200

    add another example of frequency counter

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-module.exports = validAnagram = (first, second) => {
+const validAnagram = (first, second) => {
 	// if not the same length, not anagram
-	if( first.length !== second.length) return false;
-	
+	if (first.length !== second.length) return false;
+	if (first === second ) return true;
+
 	let lookup = {};
 
 	// create frequency counter for first word
-	for( let char of first){
+	for (let char of first) {
 		lookup[char] = (lookup[char] || 0) + 1;
 	}
 
-	// loop over the second string and check 
+	// loop over the second string and check
 	// if char from the first word is exists
-	for (let char of second){
+	for (let char of second) {
 		// not found, exit. not anagram
-		if( !lookup[char] ) return false;
+		if (!lookup[char]) return false;
 
 		lookup[char] -= 1;
 	}
 
 	// anagram, all good!
 	return true;
+};
+
+const areThereDuplicates = function (){
+	if (arguments.length < 1) return false;
+
+	let lookup = {};
+
+	for (let i = 0; i < arguments.length; i ++) {
+		let val = arguments[i];
+
+		if (lookup[val]) {
+			return true;
+		}
+
+		lookup[val] = 1;
+	}
+	
+	return false;
+};
+
+module.exports = {
+	validAnagram,
+	areThereDuplicates
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,29 @@
-const validAnagram = require("../index");
+const {validAnagram} = require("../index");
 
-describe("Test if function is exists", () => {
-	it("Function exists", () => {
-		expect( validAnagram('', '')).toBeDefined();
-	})
-});
-
-describe("Anagram words", () => {
-	it("Test anagram words", () => {
-		expect(validAnagram("", "")).toEqual(true);
-		expect(validAnagram("anagram", "nagaram")).toEqual(true);
-		expect(validAnagram("qwerty", "qweyrt")).toEqual(true);
-		expect(validAnagram("texttwisttime", "timetwisttext")).toEqual(true);
+describe("Testing Valid Anagram", () => {
+	describe("Test if function is exists", () => {
+		it("Function exists", () => {
+			expect( validAnagram).toBeDefined();
+		})
 	});
-});
-
-describe("NON Anagram words", () => {
-	it("Test NON anagram words", () => {
-		expect(validAnagram("s", "")).toEqual(false);
-		expect(validAnagram("aaz", "zza")).toEqual(false);
-		expect(validAnagram("awesome", "awesom")).toEqual(false);
-		expect(validAnagram("amanaplanacanalpanama", "acanalmanplanpanama")).toEqual(false);
-		expect(validAnagram("anagrams", "nagaramm")).toEqual(false);
+	
+	describe("Anagram words", () => {
+		it("Test anagram words", () => {
+			expect(validAnagram("", "")).toEqual(true);
+			expect(validAnagram("anagram", "nagaram")).toEqual(true);
+			expect(validAnagram("qwerty", "qweyrt")).toEqual(true);
+			expect(validAnagram("texttwisttime", "timetwisttext")).toEqual(true);
+		});
+	});
+	
+	describe("NON Anagram words", () => {
+		it("Test NON anagram words", () => {
+			expect(validAnagram("s", "")).toEqual(false);
+			expect(validAnagram("aaz", "zza")).toEqual(false);
+			expect(validAnagram("awesome", "awesom")).toEqual(false);
+			expect(validAnagram("amanaplanacanalpanama", "acanalmanplanpanama")).toEqual(false);
+			expect(validAnagram("anagrams", "nagaramm")).toEqual(false);
+		});
 	});
 });
 
